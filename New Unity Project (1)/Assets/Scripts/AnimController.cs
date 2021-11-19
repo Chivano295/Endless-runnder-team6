78bdc08a160e7faed4f3cfff11d59bb5378c8f51
Chivano295 <crijsdam@hotmail.com>
2021-11-19 09:02:02 +0100

    Main menu + pauze

---
+++ New Unity Project (1)/Assets/Scripts/AnimController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    private Animator anim;
+    private int animint = 0;
+    public GameObject[] menubuttons = new GameObject[3];
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        animint = anim.GetInteger("Camerapos");
+        menubuttons[1].SetActive(false);
+        menubuttons[0].SetActive(false);
+        menubuttons[2].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animint = animint + 1;
+        }
+        anim.SetInteger("Camerapos", (animint));
+        if(animint >= 4)
+        {
+            animint = 1;       
+        }
+
+        if (animint == 0)
+        {
+            menubuttons[0].SetActive(true);
+            menubuttons[1].SetActive(false);
+            menubuttons[2].SetActive(false);
+        }
+        if (animint == 1)
+        {
+            menubuttons[0].SetActive(false);
+            menubuttons[1].SetActive(true);
+            menubuttons[2].SetActive(false);
+        }
+        if (animint == 2)
+        {
+            menubuttons[0].SetActive(false);
+            menubuttons[1].SetActive(false);
+            menubuttons[2].SetActive(true);
+        }
+        if (animint == 3)
+        {
+            menubuttons[0].SetActive(true);
+            menubuttons[1].SetActive(false);
+            menubuttons[2].SetActive(false);
+        }
+
+
+
+
+    }
+    
+
+    
+}
---
+++ New Unity Project (1)/Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviour
+{
+    
+    Animator anim;
+
+    public void Start()
+    {
+        
+    }
+    public void Update()
+    {
+        
+    }
+}
