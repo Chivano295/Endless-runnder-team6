a230873e0d9b28d36cc089ee1889aa79ad54b388
Milos Zarifovic <milos-s-markovic1994@live.nl>
2021-11-19 09:08:27 +0100

    Streamlined Segment Loop
    Streamlined code

--- New Unity Project (1)/Assets/Scripts/Segment.cs
+++ New Unity Project (1)/Assets/Scripts/Segment.cs
@@ -3,22 +3,19 @@ using UnityEngine;
 public class Segment : PoolItem
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float removeLoc;
 
-    //Moves the segment at given speed
+    //Moves the segment at given speed and check if it reaches a certain position
     private void Update()
     {
         transform.position += new Vector3(0, 0, -moveSpeed * Time.deltaTime);
 
-    }
-
-    //Checks for collision with boundary, returns to pool and calls function for next segment
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Boundary"))
+        if (transform.position.z <= removeLoc)
         {
             GameManager.Instance.NextSegment();
             GameManager.Instance.RemoveSegment(this);
         }
+
     }
 
     //Back in 2020, myself and David van Rijn worked on Endless Runner. Code similarties are likely!
